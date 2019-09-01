410d3fb6e81ba9e4614f8741b502d485c386c789
Chocobe <nice_10bird@naver.com>
2019-09-01 23:08:05 +0900

    <master> : Fixed main.js

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 * Jquery 페이지 : https://code.jquery.com (https://code.jquery.com/jquery)
 
-* CDN (Content Delivery Network : 콘텐츠 전송 네트워크) : https://cdnjs.com (https://cdnjs.com/libraries/)
+* CDN (Content Delivery Network : 콘텐츠 전송 네트워크) : https://cdnjs.com (https://cdnjs.com/libraries)
--- chapter_1_first_application/main.js
+++ chapter_1_first_application/main.js
@@ -6,8 +6,22 @@ $(document).ready(function() {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
     
-    var c = Shape.Circle(200, 200, 50);
-    c.fillColor = 'green';
+    var tool = new Tool();
+    
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'Hello World';
+    
+    tool.onMouseDown = function(event) {
+        // var c = Shape.Circle(event.point.x, event.point.y, 20);
+        var c = Shape.Circle(event.point, 20);
+        c.fillColor = 'magenta';
+    }
 
     paper.view.draw();
 
