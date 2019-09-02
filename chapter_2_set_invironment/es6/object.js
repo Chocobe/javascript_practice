cd76eebe8329a965190b14197e3f0aa96067f8f6
Chocobe <nice_10bird@naver.com>
2019-09-02 22:31:03 +0900

    <master> : Fixed object.js

--- chapter_2_set_invironment/es6/object.js
+++ chapter_2_set_invironment/es6/object.js
@@ -32,6 +32,17 @@ function printObject(obj) {
                 `${obj.classification.subfamily} ${obj.classification.genus} ${obj.classification.species}`);
 };
 
+sam3.speak = function() {
+    console.log("Meow!");
+};
+
 printing(sam1);
 printing(sam2);
 printObject(sam3);
+sam3.speak();
+
+delete sam3.classification;
+delete sam3.speak;
+
+// sam3.speak 함수가 삭제되었기 때문에, 오류발생
+// sam3.speak(); 
