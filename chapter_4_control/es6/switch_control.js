5b73e745e23febf8f757b0a58201b38a325e0d1e
Chocobe <nice_10bird@naver.com>
2019-09-03 16:10:45 +0900

    <master> : Created continue_control.js

---
+++ chapter_4_control/es6/switch_control.js
@@ -0,0 +1,11 @@
+let totalBet = 7;
+
+switch(totalBet) {
+    case 7:
+        console.log(`배팅금 : ${totalBet}`);
+        break;
+
+    case 13:
+        console.log(`배팅금 : ${totalBet}`);
+        break;
+}
