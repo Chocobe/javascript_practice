0038be249b06ae9177b4975f3c5749c4c69cf509
Chocobe <nice_10bird@naver.com>
2019-09-09 10:28:47 +0900

    <master> : Created folder 01_if_3_test

---
+++ do it javascript/03_제어문/01_if_3_test/if_3_test.js
@@ -0,0 +1,33 @@
+var val_1 = 0;
+var val_2 = "";
+var val_3 = null;
+var val_4;
+
+
+if(val_1) {
+    document.write(`${val_1} : true`, "<br>");
+
+} else {
+    document.write(`${val_1} : false`, "<br>");
+}
+
+if(val_2) {
+    document.write(`${val_2} : true`, "<br>");
+
+} else {
+    document.write(`${val_2} : false`, "<br>");
+}
+
+if(val_3) {
+    document.write(`${val_3} : true`, "<br>");
+
+} else {
+    document.write(`${val_3} : false`, "<br>");
+}
+
+if(val_4) {
+    document.write(`${val_4} : true`, "<br>");
+
+} else {
+    document.write(`${val_4} : false`, "<br>");
+}
