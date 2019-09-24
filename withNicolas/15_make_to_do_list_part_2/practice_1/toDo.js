73fb3c607207a23756226b7201c6fdea07faaa8c
Chocobe <nice_10bird@naver.com>
2019-09-24 18:46:32 +0900

    <master> Created practice_1

---
+++ withNicolas/15_make_to_do_list_part_2/practice_1/toDo.js
@@ -0,0 +1,58 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+const toDos = [];
+
+function init() {
+    loadToDoList();
+    toDoForm.addEventListener("submit", handleToDoInput);
+}
+init();
+
+function loadToDoList() {
+    const loadedToDoList = localStorage.getItem(TODOS_LS);
+
+    if(loadedToDoList !== null) {
+        const parsedDatas = JSON.parse(loadedToDoList);
+        parsedDatas.forEach(function(data) {
+            paintToDo(data.text); // data = { id:xxx , text:xxx }
+            toDos.push(data);
+        });
+    }
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li"),
+        button = document.createElement("button"),
+        span = document.createElement("span");
+
+    button.innerText = "✖️";
+    span.innerText = text;
+    li.appendChild(button);
+    li.appendChild(span);
+
+    toDoList.appendChild(li);
+}
+
+function handleToDoInput(event) {
+    event.preventDefault();
+
+    const currentId = toDos.length + 1;
+    const inputValue = toDoInput.value;
+
+    const currentToDo = {
+        id : currentId,
+        text: inputValue
+    };
+
+    toDos.push(currentToDo);
+    saveLocalStorage();
+    paintToDo(currentToDo.text);
+    toDoInput.value = "";
+}
+
+function saveLocalStorage() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
